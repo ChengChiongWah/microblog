b3fd44a9383ca57e95f179d499a25bf910df0ad6
chengchiongwah <chengchiongwah@gmail.com>
2015-07-14 21:23:49 +0800

    part8A

--- app/main/views.py
+++ app/main/views.py
@@ -6,26 +6,25 @@ from .forms import NameForm
 from .. import db
 from ..models import User
 
+
 @main.route('/', method=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
         user = User.query.fileter_by(username=form.name.data).first()
-	if user is None:
-	    user = User(username=form.name.data)
-	    db.session.add(user)
-	    session['known'] = False
-	    if current_app.config['FLASKY_ADMIN']:
-	        send_email(current_app.config['FLASKY_ADMIN'], 'New User',
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if current_app.config['FLASKY_ADMIN']:
+                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
 		           'main/new_user', user=user)
-	    else:
-	        session['know'] = True
-            session['name'] = form.name.data
-	    form.name.data =' '
-	    return redirect(url_for('.index'))
-	return render_template('index.html',
-	                       form=form, name=session.get('name'),
-			       known=session.get('known', Flase),
-			       current_time=date.utcnow())
-							   
-
+        else:
+            session['know'] = True
+        session['name'] = form.name.data
+        form.name.data =' '
+        return redirect(url_for('.index'))
+    return render_template('index.html',
+                            form=form, name=session.get('name'),
+                            known=session.get('known', Flase),
+                            current_time=date.utcnow())
