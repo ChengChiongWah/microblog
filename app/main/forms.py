8cccc8c0f23fc965ba4f08675bcd8042d01e1642
CentOS6.5 <cheng@localhost.localdomain>
2015-07-21 17:45:26 +0800

    part8c

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,4 +5,4 @@ from wtforms.validators import Required
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
-    submit = SubmitField('submit')
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -7,20 +7,20 @@ from .. import db
 from ..models import User
 
 
-@main.route('/', method=['GET', 'POST'])
+@main.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        user = User.query.fileter_by(username=form.name.data).first()
+        user = User.query.filter_by(username=form.name.data).first()
         if user is None:
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
             if current_app.config['FLASKY_ADMIN']:
                 send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-		           'main/new_user', user=user)
+                           'main/new_user', user=user)
         else:
-            session['know'] = True
+            session['known'] = True
         session['name'] = form.name.data
         form.name.data =' '
         return redirect(url_for('.index'))
