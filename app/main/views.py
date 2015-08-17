fa98cc8a1526b8c8f74ae6d052cefa224952bfe8
chengchiongwah <chengchiongwah@gmail.com>
2015-08-17 21:01:44 +0800

    13c

--- app/main/views.py
+++ app/main/views.py
@@ -157,7 +157,25 @@ def followers(username):
     return render_template('followers.html', user=user, title="Followers of", 
                             endpoint='.followers', pagination=pagination,
 			    follows=follows)
+
+
+@main.route('/followed_by/<username>')
+def followed_by(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user.')
+	return redirect(url_for('.index'))
+	page = request.args.get('pag', 1, type=int)
+	pagination = user.followed.paginate(
+	    page, per_page=current_app.config['FLASKY_FOLLOWERS_PER_PAGE'],
+	    error_out=False)
+	follows = [{'user': item.followed, 'timestamp': item.timestamp}
+	           for item in pagination.items]
+	return render_template('follower.html', user=user, title="Followed by",
+	                       endpoint='.followed_by', pagination=pagination,
+			       follows=follows)
 			
+
 @main.route('/all')
 @login_required
 def show_all():
